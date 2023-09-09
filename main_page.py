59071fab15267ab7cc79168806fa19e9fdcdc15d
Cheeze06 <74716470+Cheeze06@users.noreply.github.com>
2023-09-09 11:03:14 +0900

    Add files via upload

---
+++ main_page.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('2023 february Game Ranking')
+
+st.header('MORE INFORMATION')
+
+
+# 각 열에 하이퍼링크 추가
+st.image('https://d1.awsstatic.com/002_RG_2021_FULL_LOCKUP_RED.5627f0de6611fcc949c7966209c7b802a0724ddd.png')
+st.markdown('<a href="https://www.riotgames.com/ko">RIOT GAMES 상세 정보</a>', unsafe_allow_html=True)
+st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Nexon_Logo.svg/799px-Nexon_Logo.svg.png')
+st.markdown('<a href="https://www.nexon.com/Home/Game">NEXON 상세 정보</a>', unsafe_allow_html=True)
+st.image('https://res.cloudinary.com/linkareer/image/fetch/f_auto,q_50/https://api.linkareer.com/attachments/44829')
+st.markdown('<a href="https://kr.ncsoft.com/kr/index.do">NCSOFT 상세 정보</a>', unsafe_allow_html=True)
+
+
---
+++ pages/Mobile_Rank.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+
+st.title('2023년 2월 PC 게임 순위')
+
+path = 'C:\pythonproject\pages\Mobile.csv'
+# CSV 파일을 데이터프레임으로 읽기
+data1 = pd.read_csv(path, encoding='cp949')
+
+st.sidebar.title('Filter')
+
+# data1['장르']
+a = data1['장르']
+b = set(a)
+c = list(b)
+
+# 서비스 목록 얻기
+services = data1['서비스'].unique()
+
+# 장르 선택
+options_genre = st.sidebar.multiselect(
+    '장르로 구분하기',
+    c,
+)
+
+# 서비스 선택
+options_service = st.sidebar.multiselect(
+    '서비스로 구분하기',
+    services,
+)
+
+# 장르와 서비스로 필터링
+filtered_data = data1
+if options_genre:
+    filtered_data = filtered_data[filtered_data['장르'].isin(options_genre)]
+if options_service:
+    filtered_data = filtered_data[filtered_data['서비스'].isin(options_service)]
+
+# 데이터프레임 출력
+if not options_genre and not options_service:
+    st.dataframe(data1, use_container_width=True)
+else:
+    st.dataframe(filtered_data, use_container_width=True)
